51275e7df51ec679f957e40947a6831c9b06d682
rmere <rmere@griddynamics.com>
2024-10-08 16:04:34 -0600

    Add router configuration for root page

---
+++ src/router.jsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Root from './layout/Root';
+
+const router = createBrowserRouter([{ path: '/', element: <Root /> }]);
+
+export default router;
