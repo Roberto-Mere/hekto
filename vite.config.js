b1c3be45f6bfd497396c3a417561d6ff35ff776d
rmere <rmere@griddynamics.com>
2024-10-09 13:13:38 -0600

    Fix vite config to include tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +16,9 @@ export default defineConfig({
       include: '**/*.svg',
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 });
