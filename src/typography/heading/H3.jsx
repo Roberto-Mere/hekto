2f9f449b9b93134ac60cddf0aac52701485e6efc
rmere <rmere@griddynamics.com>
2024-10-10 10:54:26 -0600

    Fix heading components not using respective html elements

--- src/typography/heading/H3.jsx
+++ src/typography/heading/H3.jsx
@@ -1,7 +1,7 @@
 export default function H3({ children, style = '' }) {
   return (
-    <h1 className={`text-h3 font-bold capitalize tracking-heading ${style}`}>
+    <h3 className={`text-h3 font-bold capitalize tracking-heading ${style}`}>
       {children}
-    </h1>
+    </h3>
   );
 }
--- src/typography/heading/H4.jsx
+++ src/typography/heading/H4.jsx
@@ -1,7 +1,7 @@
 export default function H4({ children, style = '' }) {
   return (
-    <h1 className={`text-h4 font-bold capitalize tracking-heading ${style}`}>
+    <h4 className={`text-h4 font-bold capitalize tracking-heading ${style}`}>
       {children}
-    </h1>
+    </h4>
   );
 }
