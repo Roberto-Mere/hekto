c7b7c9c3adc787cff7d21f88c2c7f567b23f1f89
rmere <rmere@griddynamics.com>
2024-10-10 10:21:17 -0600

    Add home component and route

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from './layout/Root';
+import Home from './pages/home/Home';
 
-const router = createBrowserRouter([{ path: '/', element: <Root /> }]);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [{ index: true, element: <Home /> }],
+  },
+]);
 
 export default router;
