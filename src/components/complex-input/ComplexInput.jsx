6f4fa8f7c46a21f7a2c202e3f26a32681d6e3df3
rmere <rmere@griddynamics.com>
2024-10-14 16:06:23 -0600

    Add default values to props

--- src/components/complex-input/ComplexInput.jsx
+++ src/components/complex-input/ComplexInput.jsx
@@ -1,14 +1,18 @@
 import Button from '../Button';
 
-export default function ComplexInput({ type, placeholder, buttonContent }) {
+export default function ComplexInput({
+  type = 'text',
+  placeholder,
+  buttonContent = '',
+}) {
   return (
-    <div className="flex min-w-[32.8rem] rounded-xl border border-gray-2 p-4 focus-within:border-tertiary">
+    <form className="flex min-w-[32.8rem] rounded-xl border border-gray-2 p-4 focus-within:border-tertiary">
       <input
         type={type}
         placeholder={placeholder}
         className="ml-12 flex-1 font-lato text-sm font-normal outline-none placeholder:text-gray-3"
       />
       <Button size="small">{buttonContent}</Button>
-    </div>
+    </form>
   );
 }
