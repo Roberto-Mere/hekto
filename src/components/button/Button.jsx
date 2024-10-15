4bb01fff7ac2d5332b36182886615d4cf5d01789
rmere <rmere@griddynamics.com>
2024-10-15 11:42:40 -0600

    Change button component to pass additional props

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,8 @@
-export default function Button({ children, size }) {
+export default function Button({ children, size, ...props }) {
   return (
     <button
       className={`rounded-xl bg-primary text-white ${size === 'small' ? 'px-16 py-8' : 'px-40 py-16'}`}
+      {...props}
     >
       {children}
     </button>
