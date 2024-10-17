682179d2dbf442418d21eb94230daf8c2c1c8403
rmere <rmere@griddynamics.com>
2024-10-17 14:10:44 -0600

    Add trending component

---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchProductSlide } from '../../../http';
+import Typography from '../../../components/typography/Typography';
+import ProductCardSmall from '../../../components/ProductCardSmall';
+import Loader from '../../../components/Loader';
+import List from '../../../components/list/List';
+import Preloader from '../../../components/Preloader';
+
+export default function Trending() {
+  const slide = Math.round(Math.random() * 3);
+
+  const { data, status } = useQuery({
+    queryKey: ['trending'],
+    queryFn: () => fetchProductSlide(slide),
+  });
+  return (
+    <section className="flex flex-col items-center justify-center px-sides py-96">
+      <Typography type="h2" classes="mb-64">
+        Trending Products
+      </Typography>
+      {status === 'pending' ? (
+        <List
+          list={Array(4).fill(null)}
+          keyFn={(_, index) => index}
+          classes="gap-32 w-full"
+          itemClasses="basis-full flex"
+        >
+          {(_) => <Preloader height="h-[34.4rem]" />}
+        </List>
+      ) : (
+        <List
+          list={data}
+          keyFn={(product) => product.id}
+          classes="gap-32"
+          itemClasses="flex-1 flex h-[34.4rem]"
+        >
+          {(product) => <ProductCardSmall {...product} />}
+        </List>
+      )}
+    </section>
+  );
+}
