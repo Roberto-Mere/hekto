f1b31e22153d78a83f2c7e00b8b39869e512703e
rmere <rmere@griddynamics.com>
2024-10-17 12:14:45 -0600

    Fix product card preloader height

--- src/components/ProductCardPreloader.jsx
+++ src/components/ProductCardPreloader.jsx
@@ -1,3 +1,3 @@
 export default function ProductCardPreloader() {
-  return <div className="rounded-xl bg-gray-1 px-[15.2em] py-[15.9rem]"></div>;
+  return <div className="h-[36.8rem] w-full rounded-xl bg-gray-1"></div>;
 }
