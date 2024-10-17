c3d7263bbd7d6d0af1d896fe86cb9981cf44aae4
rmere <rmere@griddynamics.com>
2024-10-17 10:29:36 -0600

    Add props to change button bg and text colors

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,7 +1,13 @@
-export default function Button({ children, size, ...props }) {
+export default function Button({
+  children,
+  backgroundColor = 'bg-primary',
+  textColor = 'text-white',
+  size,
+  ...props
+}) {
   return (
     <button
-      className={`rounded-xl bg-primary text-white ${size === 'small' ? 'px-16 py-8' : 'px-40 py-16'}`}
+      className={`rounded-xl ${backgroundColor} ${textColor} ${size === 'small' ? 'px-16 py-8' : 'px-40 py-16'}`}
       {...props}
     >
       {children}
