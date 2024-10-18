f59f5cf8ee6269445506161fdb4d416de22d6aaa
rmere <rmere@griddynamics.com>
2024-10-18 16:10:40 -0600

    Change tabbed tests to use render functions

--- src/components/tabbed/Tabbed.test.jsx
+++ src/components/tabbed/Tabbed.test.jsx
@@ -5,119 +5,57 @@ import userEvent from '@testing-library/user-event';
 
 describe('Tabbed component', () => {
   it('should render a list of tab buttons and tab content', () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3', content: 'Tab 3' },
-    ];
+    const tabs = ['1', '2', '3'];
+    const RenderFn = ({ tab }) => <p>{`Tab ${tab}`}</p>;
 
-    render(<Tabbed tabs={tabs} />);
+    render(<Tabbed tabs={tabs} RenderFn={RenderFn} />);
 
     const buttons = screen.getAllByRole('button');
 
-    expect(buttons[0].textContent).toEqual(tabs[0].name);
-    expect(buttons[1].textContent).toEqual(tabs[1].name);
-    expect(buttons[2].textContent).toEqual(tabs[2].name);
+    expect(buttons[0].textContent).toEqual(tabs[0]);
+    expect(buttons[1].textContent).toEqual(tabs[1]);
+    expect(buttons[2].textContent).toEqual(tabs[2]);
 
-    expect(screen.getByRole('article').textContent).toEqual(tabs[0].content);
+    expect(screen.getByRole('article').textContent).toEqual('Tab 1');
+  });
+
+  it('should use render function passed to render tab content', () => {
+    const tabs = ['1', '2', '3'];
+    const RenderFn = vi.fn(({ tab }) => <h1>{`Tab ${tab}`}</h1>);
+
+    render(<Tabbed tabs={tabs} RenderFn={RenderFn} />);
+
+    expect(RenderFn).toHaveBeenCalled();
+    expect(screen.getByRole('heading').textContent).toEqual('Tab 1');
   });
 
   it('should change the tab content when clicking on a tab button', async () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3', content: 'Tab 3' },
-    ];
+    const tabs = ['1', '2', '3'];
+    const RenderFn = ({ tab }) => <p>{`Tab ${tab}`}</p>;
 
-    render(<Tabbed tabs={tabs} />);
+    render(<Tabbed tabs={tabs} RenderFn={RenderFn} />);
 
     const buttons = screen.getAllByRole('button');
 
     await userEvent.click(buttons[1]);
 
-    expect(screen.getByRole('article').textContent).toEqual(tabs[1].content);
-  });
-
-  it('should fetch the tab content of clicked tab button if it doesnt exist', async () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3' },
-    ];
-    const fetchTabContent = vi.fn(
-      () => new Promise((resolve) => resolve('Tab')),
-    );
-
-    render(<Tabbed tabs={tabs} fetchTabContent={fetchTabContent} />);
-
-    const buttons = screen.getAllByRole('button');
-
-    await userEvent.click(buttons[2]);
-
-    expect(fetchTabContent).toHaveBeenCalled();
-    expect(screen.getByRole('article').textContent).toEqual('Tab');
-  });
-
-  it('should not fetch tab content of clicked tab if it already exists', async () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3', content: 'Tab 3' },
-    ];
-    const fetchTabContent = vi.fn(
-      () => new Promise((resolve) => resolve('Tab')),
-    );
-
-    render(<Tabbed tabs={tabs} fetchTabContent={fetchTabContent} />);
-
-    const buttons = screen.getAllByRole('button');
-
-    await userEvent.click(buttons[2]);
-
-    expect(fetchTabContent).not.toHaveBeenCalled();
-  });
-
-  it('should not fetch tab content of clicked tab if it has already been fetched', async () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3' },
-    ];
-    const fetchTabContent = vi.fn(
-      () => new Promise((resolve) => resolve('Tab')),
-    );
-
-    render(<Tabbed tabs={tabs} fetchTabContent={fetchTabContent} />);
-
-    const buttons = screen.getAllByRole('button');
-
-    await userEvent.click(buttons[2]);
-    await userEvent.click(buttons[1]);
-    await userEvent.click(buttons[2]);
-
-    expect(fetchTabContent).toHaveBeenCalledOnce();
+    expect(screen.getByRole('article').textContent).toEqual('Tab 2');
   });
 
   it('should add tab button container classes when passed', () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3', content: 'Tab 3' },
-    ];
+    const tabs = ['1', '2', '3'];
+    const RenderFn = vi.fn();
 
-    render(<Tabbed tabs={tabs} buttonsClasses="test" />);
+    render(<Tabbed tabs={tabs} buttonsClasses="test" RenderFn={RenderFn} />);
 
     expect(screen.getByRole('list')).toHaveClass('test');
   });
 
   it('should not add tab button container classes when not passed', () => {
-    const tabs = [
-      { name: '1', content: 'Tab 1' },
-      { name: '2', content: 'Tab 2' },
-      { name: '3', content: 'Tab 3' },
-    ];
+    const tabs = ['1', '2', '3'];
+    const RenderFn = vi.fn();
 
-    render(<Tabbed tabs={tabs} />);
+    render(<Tabbed tabs={tabs} RenderFn={RenderFn} />);
 
     expect(screen.getByRole('list')).toHaveClass('flex', { exact: true });
   });
