4f1f68cee5676ad4b815e6662f0f9d57adb8f333
rmere <rmere@griddynamics.com>
2024-10-18 17:40:37 -0600

    Add wide product card component

---
+++ src/components/ProductCardWide.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import Typography from './typography/Typography';
+
+export default function ProductCardWide({ id, image, name, price }) {
+  return (
+    <article className="flex h-full basis-full">
+      <Link to={`/products/${id}`} className="grid-product grid basis-full">
+        <img
+          src={image}
+          alt={name}
+          className="grid-overlap mb-12 h-[27rem] w-full object-cover"
+        />
+        <div className="flex justify-between">
+          <Typography type="label" classes="text-primary mb-8 w-max">
+            {name}
+          </Typography>
+          <Typography type="label">${price.toFixed(2)}</Typography>
+        </div>
+      </Link>
+    </article>
+  );
+}
