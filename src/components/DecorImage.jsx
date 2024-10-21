323496c5c4010e70fce06f729c27f96da45f6392
rmere <rmere@griddynamics.com>
2024-10-21 08:47:34 -0600

    Adjust discount image sizes

--- src/components/DecorImage.jsx
+++ src/components/DecorImage.jsx
@@ -3,10 +3,14 @@ import Ellipse from '../assets/svg/ellipse.svg';
 
 export default function DecorImage({ src, alt }) {
   return (
-    <Link to="#" className="relative grid h-auto w-1/3">
+    <Link to="#" className="relative grid w-1/3">
       <Ellipse className="grid-overlap h-full w-full text-gray-2" />
       <Ellipse className="grid-overlap absolute -right-48 -top-32 h-3/4 w-3/4 text-gray-2" />
-      <img src={src} alt={alt} className="grid-overlap w-full translate-x-48" />
+      <img
+        src={src}
+        alt={alt}
+        className="grid-overlap h-full max-h-[43.7rem] w-full translate-x-48 self-center object-contain"
+      />
     </Link>
   );
 }
