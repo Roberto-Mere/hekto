2633a54addcc3f31539299485d72b88ba6c16c13
rmere <rmere@griddynamics.com>
2024-10-21 09:50:14 -0600

    Add products route and component

---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,11 @@
+import Typography from '../../components/typography/Typography';
+
+export default function Products() {
+  return (
+    <>
+      <Typography type="h1" classes="mt-64 mb-96 text-center">
+        Products
+      </Typography>
+    </>
+  );
+}
--- src/router.jsx
+++ src/router.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from './layout/Root';
 import Home from './pages/home/Home';
+import Products from './pages/products/Products';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: '/products', element: <Products /> },
+    ],
   },
 ]);
 
