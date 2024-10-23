aa5a3c894fca16dbec72af0e0cc767d58e9f9b76
rmere <rmere@griddynamics.com>
2024-10-23 10:33:46 -0600

    Add filters state and reducers

--- src/store/products.js
+++ src/store/products.js
@@ -1,6 +1,15 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = { view: 'list' };
+const initialState = {
+  view: 'list',
+  filters: {
+    brand: [],
+    discount: [],
+    rating: [],
+    category: [],
+    price: [],
+  },
+};
 
 export const productsSlice = createSlice({
   name: 'products',
@@ -8,6 +17,20 @@ export const productsSlice = createSlice({
   reducers: {
     changeView(state, action) {
       state.view = action.payload;
+    },
+    toggleFilter(state, action) {
+      const type = action.payload.type;
+      const id = action.payload.id;
+      const filterIndex = state.filters[type].indexOf(id);
+
+      if (filterIndex > -1) {
+        state.filters[type].splice(filterIndex, 1);
+      } else {
+        state.filters[type].push(id);
+      }
+    },
+    setFilters(state, action) {
+      state.filters = action.payload;
     },
   },
 });
