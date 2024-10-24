bd329628936031b5a3997924ffebfecfc1bf3f36
rmere <rmere@griddynamics.com>
2024-10-24 15:02:43 -0600

    Add cart state slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { productsSlice } from './products';
+import { cartSlice } from './cart';
 
-const store = configureStore({ reducer: { products: productsSlice.reducer } });
+const store = configureStore({
+  reducer: { products: productsSlice.reducer, cart: cartSlice.reducer },
+});
 
 export const productsActions = productsSlice.actions;
+export const cartActions = cartSlice.actions;
 
 export default store;
