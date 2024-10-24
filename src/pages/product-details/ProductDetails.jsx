c64ee9632e07e2ad50459ece59cc6af8df20bdef
rmere <rmere@griddynamics.com>
2024-10-24 16:03:28 -0600

    Add product details description component

---
+++ src/pages/product-details/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import Description from './description/Description';
+import Related from './related/Related';
+
+export default function ProductDetails() {
+  return (
+    <main>
+      <Description />
+      <Related />
+    </main>
+  );
+}
