cc86cc58cbfb7149774760d21de64ac15f1d23d4
rmere <rmere@griddynamics.com>
2024-10-25 16:46:02 -0600

    Add error page

--- src/router.jsx
+++ src/router.jsx
@@ -4,11 +4,13 @@ import Home from './pages/home/Home';
 import Products from './pages/products/Products';
 import Cart from './pages/cart/Cart';
 import ProductDetails from './pages/product-details/ProductDetails';
+import ErrorPage from './pages/error/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <Home /> },
       { path: '/products', element: <Products /> },
