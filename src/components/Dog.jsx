a96eff4d36330216136cc4899838855fcc93b3f7
rmere <rmere@griddynamics.com>
2024-10-31 11:33:42 -0600

    Change dog image to include text in sign

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -3,14 +3,8 @@ import Typography from './typography/Typography';
 
 export default function Dog() {
   return (
-    <div className="dog absolute left-48 top-[50rem]">
+    <div className="dog absolute -bottom-[78rem] left-48">
       <img src={dog} alt="Dog holding sign" className="h-[26vh]" />
-      <Typography
-        type="sub1"
-        classes="absolute bottom-[7.2rem] left-64 max-w-[8vw] text-center"
-      >
-        Please turn off the light!
-      </Typography>
     </div>
   );
 }
--- src/components/Lamp.jsx
+++ src/components/Lamp.jsx
@@ -18,6 +18,7 @@ export default function Lamp() {
     if (isTurnedOn) {
       dogTimer.current = setTimeout(() => setShowDog(true), 7000);
     }
+    return () => clearTimeout(dogTimer.current);
   }, [isTurnedOn]);
 
   return (
@@ -26,7 +27,7 @@ export default function Lamp() {
         <img
           src={headlamp}
           alt="Headlamp"
-          className="peer absolute -top-128 z-10 h-[35vh] w-auto transition-all duration-700 ease-in-out hover:top-0"
+          className="peer absolute -top-128 z-10 h-[38rem] w-auto transition-all duration-700 ease-in-out hover:top-0"
         />
         {isTurnedOn ? (
           <Light className="absolute left-12 top-[20rem] text-background opacity-75 transition-all duration-700 ease-in-out peer-hover:top-[32rem]" />
